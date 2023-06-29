44a1d8b6ce57098aae2a2edc31c6d7f552ec942c
紫竹 <luozhu2021@gmail.com>
2023-06-29 13:44:46 +0800

    Update .dumirc.ts

--- .dumirc.ts
+++ .dumirc.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   favicons: ['https://cdn.jsdelivr.net/gh/youngjuning/images@main/1687963061740.png'],
   autoAlias: false,
   themeConfig: {
-    name: 'React DnD',
+    name: 'React DnD 中文',
     logo: 'https://cdn.jsdelivr.net/gh/youngjuning/images@main/1687963061740.png',
     prefersColor: { default: 'auto' },
     socialLinks: {
