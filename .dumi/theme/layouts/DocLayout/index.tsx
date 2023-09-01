8ae3ee1d5e0ca16a20c04a21915bc1c9a9188bb6
紫竹 <luozhu2021@gmail.com>
2023-09-01 22:26:46 +0800

    Update index.tsx

--- .dumi/theme/layouts/DocLayout/index.tsx
+++ .dumi/theme/layouts/DocLayout/index.tsx
@@ -90,31 +90,15 @@ const DocLayout: FC = () => {
       <main>
         {showSidebar && <Sidebar />}
         <Content>
-          <div className='markdown' style={{ marginBottom: '18px'}}>
-            <center>
-              <a href="https://cloud.tencent.com/act/cps/redirect?redirect=1040&cps_key=ea8068199c73a836ffe37e4ea4c2ddee&from=console" nofollow alt="腾讯云新客专属福利" target="_blank">腾讯云新客专属福利</a> | <a href="https://www.cnouyi.careers/join/54171828" nofollow alt="欧易 - 注册开盲盒赢 BTC、ETH、DOGE" target="_blank">欧易 - 注册开盲盒赢 BTC、ETH、DOGE</a> | <a href="https://sockboom.shop/auth/register?affid=552052" nofollow alt="SockBoom - 科学上网 VPN 梯子" target="_blank">SockBoom 稳定的科学上网梯子</a> | <a href="https://youngjuning.js.org/categories/Google/Adsense/" nofollow alt="博客网赚变现">博客网赚变现</a>
-            </center>
-          </div>
           {outlet}
-          {pathname === '/' &&
-            <Adsense
-              className="adsbygoogle"
-              style={{ display: "block" }}
-              data-ad-client="ca-pub-7029815294762181"
-              data-ad-slot="2148104191"
-              data-ad-format="auto"
-              data-full-width-responsive="true"
-            />
-          }
-          {pathname !== '/' &&
-            <Adsense
-              className="adsbygoogle"
-              style={{ display: "block" }}
-              data-ad-format="autorelaxed"
-              data-ad-client="ca-pub-7029815294762181"
-              data-ad-slot="4521975302"
-            />
-          }
+          <Adsense
+            className="adsbygoogle"
+            style={{ display: "block" }}
+            data-ad-client="ca-pub-7029815294762181"
+            data-ad-slot="6412968057"
+            data-ad-format="auto"
+            data-full-width-responsive="true"
+          />
           <Footer />
         </Content>
         {fm.toc === 'content' && (
