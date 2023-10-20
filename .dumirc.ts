9415fe98609f8f6e1e7a0c74032c59f8ca96cdf6
紫升 <luozhu2021@gmail.com>
2023-10-20 08:34:55 +0800

    Update .dumirc.ts

--- .dumirc.ts
+++ .dumirc.ts
@@ -12,7 +12,7 @@ export default defineConfig({
       twitter: 'https://twitter.com/luozhu2021'
     },
     hd: { rules: [] },
-    footer: 'Made with ❤️ by <a href="https://youngjuning.js.org/zizhudocs" target="_blank" nofollow>紫竹翻译计划</a>'
+    footer: 'Made with ❤️ by <a href="https://youngjuning.js.org/" target="_blank" nofollow>紫竹翻译计划</a>'
   },
   theme: {
     '@c-primary': '#4078c0',
